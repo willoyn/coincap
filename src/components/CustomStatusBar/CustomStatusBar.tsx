427c38ba2ac60f5412231ce7e84b672a0b058d4e
Anton Zhuk <wloynmail@gmail.com>
2022-06-21 21:00:48 +0300

    CustomStatusBar added

---
+++ src/components/CustomStatusBar/CustomStatusBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { StatusBar } from 'react-native'
+
+const CustomStatusBar = () => (
+  <StatusBar barStyle="dark-content" backgroundColor="#e5e4e2" />
+)
+
+export default CustomStatusBar
