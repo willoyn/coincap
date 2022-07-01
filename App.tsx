e4a9ea1582867c28d5d8612adeb9ee4ea15d786f
Anton <wloynmail@gmail.com>
2022-07-01 20:50:34 +0300

    apollo-client and graphql installed

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { AppContextProvider } from './src/store/index'
 import Navigation from './src/navigation/Navigation'
 
-const App = () => (
-  <AppContextProvider>
-    <Navigation />
-  </AppContextProvider>
-)
+const App = () => {
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache(),
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <AppContextProvider>
+        <Navigation />
+      </AppContextProvider>
+    </ApolloProvider>
+  )
+}
 
 export default App
